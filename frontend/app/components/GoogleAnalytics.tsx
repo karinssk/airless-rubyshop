76c2018c21ea491848c279f49d17815ed561adcd
trynotcatcherror <garinsookkalya@gmail.com>
2026-02-10 16:14:06 +0700

    test

--- frontend/app/components/GoogleAnalytics.tsx
+++ frontend/app/components/GoogleAnalytics.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import { useEffect } from "react";
 import Script from "next/script";
 
 const GA_MEASUREMENT_ID =
   process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-0PWGSWH0P4";
+const GA_DEBUG = process.env.NEXT_PUBLIC_GA_DEBUG === "true";
 
 export default function GoogleAnalytics() {
   if (!GA_MEASUREMENT_ID) {
     return null;
   }
+
+  useEffect(() => {
+    if (!GA_DEBUG) return;
+    // Debug-only log to verify GA tag injection on client.
+    // eslint-disable-next-line no-console
+    console.log("[GA] Loaded", { id: GA_MEASUREMENT_ID });
+  }, []);
 
   return (
     <>
--- frontend/app/components/TrackingScripts.tsx
+++ frontend/app/components/TrackingScripts.tsx
@@ -10,6 +10,7 @@ const META_PIXEL_ID = "1559144322039457";
 export default function TrackingScripts() {
   const [enabled, setEnabled] = useState(false);
   const isProd = process.env.NODE_ENV === "production";
+  const gaEnabled = isProd; // Temporarily load GA without consent
 
   useEffect(() => {
     if (!isProd) return;
@@ -24,30 +25,32 @@ export default function TrackingScripts() {
     return () => window.removeEventListener("cookieConsentAccepted", handleConsent);
   }, [isProd]);
 
-  if (!enabled) return null;
+  if (!enabled && !gaEnabled) return null;
 
   return (
     <>
-      {/* Google Tag Manager */}
-      <Script
-        id="gtm-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      {enabled && (
+        <>
+          {/* Google Tag Manager */}
+          <Script
+            id="gtm-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`,
-        }}
-      />
-      {/* End Google Tag Manager */}
+            }}
+          />
+          {/* End Google Tag Manager */}
 
-      {/* Meta Pixel Code */}
-      <Script
-        id="meta-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `!function(f,b,e,v,n,t,s)
+          {/* Meta Pixel Code */}
+          <Script
+            id="meta-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -57,11 +60,13 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '${META_PIXEL_ID}');
 fbq('track', 'PageView');`,
-        }}
-      />
-      {/* End Meta Pixel Code */}
+            }}
+          />
+          {/* End Meta Pixel Code */}
+        </>
+      )}
 
-      <GoogleAnalytics />
+      {gaEnabled && <GoogleAnalytics />}
     </>
   );
 }
