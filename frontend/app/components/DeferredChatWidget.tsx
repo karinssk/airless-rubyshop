4c61cd07e7bdb49d30280bc6b8ae570332f03638
trynotcatcherror <garinsookkalya@gmail.com>
2026-02-12 10:23:21 +0700

    add new chat widget

--- frontend/app/components/DeferredChatWidget.tsx
+++ frontend/app/components/DeferredChatWidget.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import Script from "next/script";
 
-const DeferredChatWidget = dynamic(() => import("./ChatWidget"), {
-  ssr: false,
-  loading: () => null,
-});
-
-export default DeferredChatWidget;
+export default function DeferredChatWidget() {
+  return (
+    <Script
+      id="tawk-to"
+      strategy="lazyOnload"
+      dangerouslySetInnerHTML={{
+        __html: `
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/67ee6174ae5da2190a19f5ef/1intipbq1';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+          })();
+        `,
+      }}
+    />
+  );
+}
