62ca09f92c962a614d5e4c8909c79a013db6256a
trynotcatcherror <garinsookkalya@gmail.com>
2026-02-18 11:24:45 +0700

    test

--- frontend/app/components/TrackingScripts.tsx
+++ frontend/app/components/TrackingScripts.tsx
@@ -5,7 +5,6 @@ import Script from "next/script";
 import GoogleAnalytics from "./GoogleAnalytics";
 import { backendBaseUrl } from "@/lib/urls";
 
-const GTM_ID = "GTM-N6CNLKHW";
 const META_PIXEL_ID = "1559144322039457";
 const FB_PAGE_ID = "816184855086392";
 
@@ -90,20 +89,6 @@ export default function TrackingScripts() {
 
       {enabled && (
         <>
-          {/* Google Tag Manager */}
-          <Script
-            id="gtm-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`,
-            }}
-          />
-          {/* End Google Tag Manager */}
-
           {/* Meta Pixel Code */}
           <Script
             id="meta-pixel"
